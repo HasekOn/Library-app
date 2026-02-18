e143856cc6a503d94ba00a5698e83b058c3cf798
Ondřej Hašek <140166751+HasekOn@users.noreply.github.com>
2026-02-18 17:36:26 +0100

    feat(UserTest): add role column to users with isLibrarian/isReader methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -44,5 +45,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isLibrarian(): bool
+    {
+        return $this->role === 'librarian';
+    }
+
+    public function isReader(): bool
+    {
+        return $this->role === 'reader';
     }
 }
